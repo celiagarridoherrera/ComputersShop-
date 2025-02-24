1eb1f20bacd421fe160a3cd7d371da53c2cb3f25
Celia Garrido <celiagarridoherrera@gmail.com>
2025-02-24 23:32:35 +0100

    creado test ConstructorTienda, el test falla

---
+++ computershop/src/test/java/model/TiendaTest.java
@@ -0,0 +1,27 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+
+public class TiendaTest {
+
+    @Test
+    public void testContrustorTienda(){
+        
+        String nombre = "PcComponentes";
+        String propietario = "Juan";
+        String idTributario = "123456789";
+
+        Tienda tienda = new Tienda(nombre, propietario, idTributario);
+
+        assertEquals(nombre, tienda.getNombre());
+        assertEquals(propietario, tienda.getPropietario());
+        assertEquals(idTributario, tienda.getIdTributario());
+        assertTrue(tienda.getOrdenadores().isEmpty());
+        
+    }
+
+}
